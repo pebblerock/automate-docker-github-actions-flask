2906fd6d5b870d5716a323d57002361e20a8a72f
pebblerock <23544798+pebblerock@users.noreply.github.com>
2023-07-01 18:27:49 +0100

    deletion of files

--- flask_app/app.py
+++ flask_app/app.py
@@ -88,9 +88,5 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/resources/')
-def resources():
-    return render_template('resources.html')
-
 if __name__ == '__main__':
     app.run()
